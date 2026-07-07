db8960cf2644694cdf6f419764fcffadcefdf614
poinalex01 <poinalex01@gmail.com>
2026-07-07 22:19:00 +0200

    Add priority info to createOrder

--- OrderService.java
+++ OrderService.java
@@ -1,7 +1,7 @@
 public class OrderService {
 
     public void createOrder(String productName, int quantity) {
-        System.out.println("Order created: " + quantity + "x " + productName);
+        System.out.println("Order created: " + quantity + "x " + productName + " (priority: normal)");
     }
 
     public void logOrderStatus(String orderId, String status) {
